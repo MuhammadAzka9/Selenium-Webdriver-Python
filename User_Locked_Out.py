ba055d47d778beb7373bf09aad955ac9b2ad1831
MuhammadAzka9 <kingazka99@gmail.com>
2024-02-09 14:07:34 +0700

    add Selenium Webdriver Python
    Signed-off-by: MuhammadAzka9 <kingazka99@gmail.com>

---
+++ User_Locked_Out.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import chromedriver_autoinstaller
+
+chromedriver_autoinstaller.install()
+
+# Inisialisasi WebDriver untuk Chrome
+webdriver = webdriver.Chrome()
+
+# Buka halaman Saucedemo
+webdriver.get("https://www.saucedemo.com")
+
+# Masukkan username dan password
+username_field = webdriver.find_element(By.ID, "user-name")
+password_field = webdriver.find_element(By.ID, "password")
+username_field.send_keys("locked_out_user")
+password_field.send_keys("secret_sauce")
+
+# Submit form login
+password_field.send_keys(Keys.RETURN)
+
+# Tunggu hingga halaman beranda muncul
+try:
+    # Assertion: Memastikan bahwa pesan kesalahan muncul setelah login gagal
+    error_message = WebDriverWait(webdriver, 20).until(
+        EC.presence_of_element_located((By.XPATH, "//h3[text()='Epic sadface: Sorry, this user has been locked out.']"))
+    )
+    print("Assertion berhasil! Pesan kesalahan muncul: ", error_message.text)
+
+    time.sleep(5)
+    
+except Exception as e:
+    print(f"Assertion Error: {e}")
+finally:
+    # Tutup browser
+    webdriver.quit()
